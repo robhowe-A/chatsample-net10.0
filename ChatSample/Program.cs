f0e34f89229b711ef05c901fe6a7f04943217b1b
Robert A Howell <rhowell91@live.com>
2026-03-17 18:06:44 -0500

    Initial commit

---
+++ ChatSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using ChatSample.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseFileServer();
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapHub<ChatHub>("/chat");
+
+
+app.Run();
