053a81307391ffc431be19cf749a108909911334
robhowe-A <rhowell91@live.com>
2026-03-17 19:58:09 -0500

    project settings and runtime logging additions

--- ChatSample/Program.cs
+++ ChatSample/Program.cs
@@ -1,4 +1,5 @@
-﻿using ChatSample.Hubs;
+﻿using System;
+using ChatSample.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -6,6 +7,11 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpLogging(options => 
+    options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All);
+builder.Services.AddW3CLogging(options =>
+    options.LoggingFields = Microsoft.AspNetCore.HttpLogging.W3CLoggingFields.All);
+
 builder.Services.AddSignalR();
 
 var app = builder.Build();
@@ -13,6 +19,23 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+
+Console
+    .WriteLine(
+        "{0}: {1}\n"+
+        "{2}: {3}\n"+ 
+        "|\t{4, -20}"+
+        "|\t{5, -20}\t|",
+        "ContentRoot Path", builder.Environment.ContentRootPath,
+        "WebRootPath", builder.Environment.WebRootPath,
+        "Application Name", "Environment Name");
+Console
+    .WriteLine(
+        $"|\t{builder.Environment.ApplicationName, -20}"+
+        $"|\t{builder.Environment.EnvironmentName, -20}\t|");
+
+app.UseHttpLogging();
+app.UseW3CLogging();
 
 app.UseFileServer();
 
